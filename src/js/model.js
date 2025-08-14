74d3ffc2c6d98f7b2d101586fe8531f962ea75bd
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-08-14 15:32:47 +0200

    feat: add bookmark management with addBookmark/deleteBookmark

--- src/js/model.js
+++ src/js/model.js
@@ -9,6 +9,7 @@ export const state = {
         resultsPerPage: RES_PER_PAGE,
         page: 1,
     },
+    bookmarks: [],
 };
 
 export const loadRecipe = async function (id) {
@@ -25,6 +26,9 @@ export const loadRecipe = async function (id) {
             cookingTime: recipe.cooking_time,
             ingredients: recipe.ingredients,
         };
+
+        if (state.bookmarks.some(bookmark => bookmark.id === id)) state.recipe.bookmarked = true;
+        else state.recipe.bookmarked = false;
     } catch (err) {
         throw err;
     }
@@ -64,4 +68,21 @@ export const updateServings = function (newServings) {
     });
 
     state.recipe.servings = newServings;
+};
+
+export const addBookmark = function (recipe) {
+    //Add bookmark
+    state.bookmarks.push(recipe);
+
+    // Mark current recipe as bookmark
+    if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+};
+
+export const deleteBookmark = function (id) {
+    // Delete bookmark
+    const index = state.bookmarks.findIndex(el => el.id === id);
+    state.bookmarks.splice(index, 1);
+
+    // Mark current recipe as NOT bookmark
+    if (id === state.recipe.id) state.recipe.bookmarked = false;
 };
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -11,13 +11,21 @@ class RecipeView extends View {
         ["hashchange", "load"].forEach(eventName => window.addEventListener(eventName, handler));
     }
 
-    addHandelUpdateServings(handler) {
+    addHandlerUpdateServings(handler) {
         this._parentElement.addEventListener("click", function (e) {
             const btn = e.target.closest(".btn--update-servings");
             if (!btn) return;
 
             const updateTo = +btn.dataset.updateTo;
             if (updateTo > 0) handler(updateTo);
+        });
+    }
+
+    addHandlerAddBookmark(handler) {
+        this._parentElement.addEventListener("click", function (e) {
+            const btn = e.target.closest(".btn--bookmark");
+            if (!btn) return;
+            handler();
         });
     }
 
@@ -64,9 +72,9 @@ class RecipeView extends View {
                 <use href="${icons}#icon-user"></use>
                 </svg>
             </div>
-            <button class="btn--round">
+            <button class="btn--round btn--bookmark">
                 <svg class="">
-                <use href="${icons}#icon-bookmark-fill"></use>
+                <use href="${icons}#icon-bookmark${this._data.bookmarked ? "-fill" : ""}"></use>
                 </svg>
             </button>
             </div>
